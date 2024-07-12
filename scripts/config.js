2e5a390bcd1d8c692141089983a45b367edd5bfe
VanessaT30 <116221188+VanessaT30@users.noreply.github.com>
2024-07-12 16:45:04 +0100

    Update config.js
    Put in functions for saving player names

--- scripts/config.js
+++ scripts/config.js
@@ -1,1 +1,22 @@
+function openPlayerConfig() {
+    playerConfigOverlay.style.display = 'block';
+    backdropElement.style.display = 'block';
+}
+
+function closePlayerConfig() {
+    playerConfigOverlay.style.display = 'none';
+    backdropElement.style.display = 'none';
+}
+
+function savePlayerConfig (event) {
+    event.preventDefault();
+    console.log(event);
+
+    // const formData = {};
+    // instantiating an object based on an object blueprint
+    // .target points at the HTML element
+    const formData = new FormData(event.target);
+    const enteredPlayername = formData.get('playername');
+    console.log(enteredPlayername);
+}
 
