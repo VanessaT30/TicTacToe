ecc03edfc2ae76efe3ecbef015fd956bf9ba256e
VanessaT30 <116221188+VanessaT30@users.noreply.github.com>
2024-07-12 16:44:25 +0100

    Update app.js
    Updated app.js to save form validation

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,30 @@
-const express = require('express');  //1st
-const app = express(); //2nd
+// const express = require('express');  //1st
+// const app = express(); //2nd
 
-const ejs = require('ejs');
+// const ejs = require('ejs');
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
 
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
 
-app.get("/index", (req, res) => {
-    res.render("index");
-  });
+// app.get("/index", (req, res) => {
+//     res.render("index");
+//   });
+
+const playerConfigOverlay = document.getElementById('config-overlay');
+const backdropElement = document.getElementById('backdrop');
+const formElement = document.querySelector('form');
+
+const editPlayerO1btn = document.getElementById ('edit-player-1');
+const editPlayerO2btn = document.getElementById ('edit-player-2');
+const cancelconfigbtn = document.getElementById ('cancel-config-btn');
+
+
+//NOTE: No parentheses added here! We DO NOT want to execute it straight away!
+editPlayerO1btn.addEventListener('click', openPlayerConfig);
+editPlayerO2btn.addEventListener('click', openPlayerConfig);
+
+cancelconfigbtn.addEventListener('click', closePlayerConfig);
+backdropElement.addEventListener('click', closePlayerConfig);
+formElement.addEventListener('submit', savePlayerConfig);
