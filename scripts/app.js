fabab319e950ba458bb88f983c22253e4b0bf075
VanessaT30 <116221188+VanessaT30@users.noreply.github.com>
2025-09-15 10:51:45 +0100

    Update app.js for game and config .js

--- scripts/app.js
+++ scripts/app.js
@@ -12,6 +12,58 @@
 //     res.render("index");
 //   });
 
+<<<<<<< HEAD
+const playerConfigOverlay = document.getElementById("modal")
+const backdropElement = document.getElementById("backdrop")
+
+const editPlayerO1 = document.getElementById ("edit-player-1");
+const editPlayerO2 = document.getElementById ("edit-player-2");
+const resetGame = document.getElementById ("reset");
+const restartGame = document.getElementById ("restart");
+
+//NOTE: No parentheses added here! We DO NOT want to execute it straight away!
+editPlayerO1.addEventListener("click", openPlayerConfig);
+editPlayerO2.addEventListener("click", openPlayerConfig);
+
+cancel.addEventListener("click", closeConfig);
+
+startGame.addEventListener('click', start);
+resetGame.addEventListener('click', reset);
+restartGame.addEventListener('click', restart);
+
+currentRound = 1;
+const gameData = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+];
+
+// let activePlayer = 0;
+// let currentRound = 1;
+// let gameIsOver = false;
+
+// const gameBoardElement = document.getElementById("game-board");
+// const activePlayerNameElement = document.getElementById("active-player-name");
+
+const players = [
+    {
+        name: "",
+        symbol: "X"
+    },
+    {
+        name: "",
+        symbol: "O"
+    }
+];
+
+// function resetGameStatus() {
+//     activePlayer = 0;
+//     currentRound = 1;
+//     gameIsOver = false;
+//     gameOverElement.firstElementChild.innerHTML = 'You won, <span id="winner-name">PLAYER NAME</span>!';
+//     gameOverElement.style.display = "none";
+// }
+=======
 const playerConfigOverlay = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
 const formElement = document.querySelector('form');
@@ -28,3 +80,4 @@ editPlayerO2btn.addEventListener('click', openPlayerConfig);
 cancelconfigbtn.addEventListener('click', closePlayerConfig);
 backdropElement.addEventListener('click', closePlayerConfig);
 formElement.addEventListener('submit', savePlayerConfig);
+>>>>>>> 247a037a7f2b45e3ed439c3a29f294ea4ae82c94
