50e82440994d4831d4eb406c427b3c771b7f3164
VanessaT30 <116221188+VanessaT30@users.noreply.github.com>
2025-11-16 14:33:13 +0000

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');  //1st
-const app = express(); //2nd
-
-const ejs = require('ejs');
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
